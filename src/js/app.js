fb97dfc1f555a328d64e95e913dd7dec8e672793
GLM-Alyona <glmalyona@gmail.com>
2023-06-08 21:14:08 +0300

    Первое и второе задание

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+import Bowerman from './Bowerman';
+import Swordsman from './Swordsman';
+import Magician from './Magician';
+import Daemon from './Daemon';
+import Undead from './Undead';
+import Zombie from './Zombie';
+
+Bowerman();
+Swordsman();
+Magician();
+Daemon();
+Undead();
+Zombie();
+
+/* const pl = new Zombie('lena');
+alert(`создали нового ${pl.name}
+${pl.type}
+${pl.health}
+${pl.level}
+${pl.attack}
+${pl.defence}
+`);
+pl.damage(15);
+alert(`нанесли урон 15 ${pl.name}
+${pl.type}
+${pl.health}
+${pl.level}
+${pl.attack}
+${pl.defence}
+`);
+pl.levelUp();
+alert(`повысили уровень ${pl.name}
+${pl.type}
+${pl.health}
+${pl.level}
+${pl.attack}
+${pl.defence}
+`);
+*/
